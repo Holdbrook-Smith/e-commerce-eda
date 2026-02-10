34bf5064dcc8a7e34ed5f9bb56d611bd4b329b7c
Holdbrook-Smith <smithholdbrook453@gmail.com>
2026-02-10 15:49:29 +0000

    Update2 portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
-import seaborn as sns
+#import altair as alt
+#import seaborn as sns
 #import os
 import plotly.express as px
 import datetime as dt
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 tab1,tab2 = st.tabs(["E-C EDA","OVERVIEW"])
 with tab1:
-
-    st.set_page_config(page_title='E-commerce shop',layout = 'wide')
-
+    st.set_page_config(page_title='E-commerce shop',layout="wide")
     st.title("Holdbrook's E-commerce EDA")
+
     fl = st.file_uploader(":file_folder: upload file")
     if fl is not None:
         filename=fl.name
@@ -47,36 +46,39 @@ with tab1:
         df2 = df[df["region"].isin(region)]
     category_df = df2.groupby('category', as_index=False)['quantity'].sum()
     with col1:
-	st.subheader("Quantity purchased")
-        fig = px.bar(category_df,x = "category", y = "quantity", text = ['${:,.2f}'.format(x) for x in category_df["quantity"]],template = "seaborn")
-	figure2.update_layout(width=1400,height=600,autosize=False)
-        st.plotly_chart(figure2,use_container_width= False, height = 200)
+        st.subheader("Quantity purchased")
+        fig = px.bar(category_df,x = "category", y = "quantity",text=['${:,.2f}'.format(x) for x in category_df["quantity"]],template = "seaborn")
+        fig.update_layout(width=1400,height=600,autosize=False)           
+        st.plotly_chart(fig,use_container_width=False)
+
+
+        
     with col2:
         st.header("Regional Purchasing Power")
         region_df = df2.groupby('region', as_index=False)['quantity'].sum()
         fig = px.pie(region_df, values = "quantity", names = "region", hole = 0.5)
         fig.update_traces(textposition = "outside")
-	figure.update_layout(width=1400,height=600,autosize=False)
-        st.plotly_chart(figure,use_container_width= False,height = 200)
+        st.plotly_chart(fig,use_container_width=True,height = 200)
     #tree based on category,region and payment_method
     st.subheader("Heirachiel Map")
     fig3 = px.treemap(df2,path = ["region","category","payment_method"],values = "quantity", hover_data=["quantity"],
                      color = "payment_method")
-    fig3.update_layout(width = 800, height = 650)
-    st.plotly_chart(fig3,use_container_width= False)
+    fig3.update_layout(width=1400,height=600,autosize=False)
+    st.plotly_chart(fig3,use_container_width = False)
     #quantity vrs profit_margin
     #col1,col2 = st.columns(2)
     #with col1:
     #    pfm = df2.groupby("quantity")["profit_margin"].value_counts().reset_index(name="count")
     #    st.line_chart(pfm)
-    #with col2:
+    #with col2
     #    pfm1 = df2.groupby("category")["profit_margin"].value_counts().reset_index(name="count")
     #    st.bar_chart(pfm1,x="category",y="profit_margin")
     st.subheader("Profit Made Over Time")
     df2["month"] = df2["order_date"].dt.to_period("M")
     plots = pd.DataFrame(df2.groupby(df2["month"].dt.strftime("%Y:%b"))["profit_margin"].sum()).reset_index()
     figure = px.line(plots,x = "month",y = "profit_margin", labels = {"profit_margin:amount"},height = 500, width = 1000, template = "gridon")
-	st.plotly_chart(figure,use_container_width = False)
+    figure.update_layout(width=1400,height=600,autosize=False)
+    st.plotly_chart(figure,use_container_width=False)
         
 
 
